c43f2cdb0b0a235fe7f2c171da99d22540275f6b
Jose Luis <sisjosex@gmail.com>
2024-08-19 21:22:21 -0400

    hide password from json responses

--- models/user.go
+++ models/user.go
@@ -11,7 +11,7 @@ type User struct {
 	FirstName string     `gorm:"column:first_name" json:"first_name"`
 	LastName  string     `gorm:"column:last_name" json:"last_name"`
 	Email     string     `gorm:"column:email;unique" json:"email" conform:"trim,lowercase"`
-	Password  string     `gorm:"column:password" json:"password"`
+	Password  string     `gorm:"column:password" json:"-"`
 	Phone     string     `gorm:"column:phone" json:"phone"`
 	Birthday  *time.Time `gorm:"column:birthday" json:"birthday"`
 }
